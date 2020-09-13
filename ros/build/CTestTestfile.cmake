b1c40b100b970fbd9fd8a556e3bf54a31a7e563f
kuberlog <collinalexbell@gmail.com>
2020-09-13 03:38:40 -0500

    ROS

---
+++ ros/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kuberlog/code/colleague-bot/body/urdf/src
+# Build directory: /home/kuberlog/code/colleague-bot/body/urdf/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
